95de511359948d8ab7f9e0a4059e126d7490c01b
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2021-06-11 23:20:12 -0500

    addded project next js

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Container from '../components/Container'
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div>
+      <Container>
+      <Head>
+          <title>about Title</title>
+        </Head>
+        <h1>about</h1>
+      </Container>
+    </div>
+  )
+}
---
+++ pages/contact.tsx
@@ -0,0 +1,16 @@
+import Container from '../components/Container'
+import Head from 'next//head'
+
+export default function Contact() {
+  return (
+    <div>
+      <Container>
+        <Head>
+          <title>Contact Title</title>
+        </Head>
+        <h1>Contact</h1>
+      </Container>
+
+    </div>
+  )
+}
