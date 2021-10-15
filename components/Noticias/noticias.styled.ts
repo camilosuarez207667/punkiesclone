a4e868b5cc550a7e44d8f56abdb3cbe9a0f05b8d
Alejandro Yunes <alejandro.yunes@inxeption.com>
2021-10-15 10:05:37 -0500

    fixed mobile

--- components/Noticias/noticias.styled.ts
+++ components/Noticias/noticias.styled.ts
@@ -4,16 +4,26 @@ export const Wrapper = styled.div`
   max-width: ${({ theme }) => theme.breakpoint.lg};
   margin: 0 auto;
   display: flex;
+  @media (max-width: ${({ theme }) => theme.breakpoint.lg}) {
+    flex-direction: column;
+  }
 `;
 
 export const NewsWrapper = styled.div`
   width: 50%;
+  padding: 8px 48px;
+  align-items: center;
   @media (max-width: ${({ theme }) => theme.breakpoint.lg}) {
-    padding: 0 24px;
+    margin: 0 auto;
+    width: auto;
+    flex-direction: column;
+    align-items: center;
   }
 `;
 
-export const ImageHeading = styled.div``;
+export const ImageHeading = styled.div`
+  max-width: 600px;
+`;
 
 export const Title = styled.div`
   font-size: ${({ theme }) => theme.fontSizes["3xl"]};
@@ -27,9 +37,8 @@ export const Description = styled.div`
   font-size: ${({ theme }) => theme.fontSizes.lg};
   color: ${({ theme }) => theme.colors.secondary};
   font-family: ${({ theme }) => theme.fonts.secondary};
-  margin: 0 auto;
   padding: 8px 0;
-  width: 100%;
+  max-width: 600px;
 `;
 
 export const Date = styled.div`
